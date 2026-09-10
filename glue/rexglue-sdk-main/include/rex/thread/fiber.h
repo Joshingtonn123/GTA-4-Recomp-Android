8eb54693562858d1f489bbfa14e8b334768b9b29
SyrexGenesisX <dlbren7775@gmail.com>
2026-09-10 11:45:28 +0000

    Select Android fiber backend

--- glue/rexglue-sdk-main/include/rex/thread/fiber.h
+++ glue/rexglue-sdk-main/include/rex/thread/fiber.h
@@ -14,7 +14,7 @@
 #include <rex/platform.h>
 #include <cstddef>
 
-#if REX_PLATFORM_LINUX || REX_PLATFORM_MAC
+#if (REX_PLATFORM_LINUX && !REX_PLATFORM_ANDROID) || REX_PLATFORM_MAC
 #if REX_PLATFORM_MAC && !defined(_XOPEN_SOURCE)
 // Darwin hides the deprecated ucontext APIs unless _XOPEN_SOURCE is defined
 // before including <ucontext.h>.
@@ -23,6 +23,11 @@
 #include <ucontext.h>
 #include <cstdint>
 #include <vector>
+#endif
+
+#if REX_PLATFORM_ANDROID
+#include <csetjmp>
+#include <cstdint>
 #endif
 
 namespace rex::thread {
@@ -56,6 +61,16 @@ struct Fiber {
 #if REX_PLATFORM_WIN32
   void* handle_ = nullptr;
   bool is_thread_fiber_ = false;
+#elif REX_PLATFORM_ANDROID
+  alignas(16) unsigned char jmpbuf_[256]{};
+  void* stack_ = nullptr;
+  size_t stack_size_ = 0;
+  void (*entry_)(void*) = nullptr;
+  void* arg_ = nullptr;
+  bool is_thread_fiber_ = false;
+  bool started_ = false;
+
+  static void Trampoline();
 #elif REX_PLATFORM_LINUX || REX_PLATFORM_MAC
   ucontext_t context_{};
   std::vector<uint8_t> stack_;
